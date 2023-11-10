56c5ae50baec095ca5737425c921881304d9801c
peter88213 <peter.88213@t-online.de>
2023-11-10 21:06:44 +0100

    Change build path to novxlib-Alpha

--- tools/build_novelyst_updater.py
+++ tools/build_novelyst_updater.py
@@ -12,7 +12,7 @@ License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 import os
 import sys
-sys.path.insert(0, f'{os.getcwd()}/../../novxlib/src')
+sys.path.insert(0, f'{os.getcwd()}/../../novxlib-Alpha/src')
 import inliner
 
 SRC = '../src/'
@@ -26,7 +26,7 @@ os.makedirs(BUILD, exist_ok=True)
 def main():
     inliner.run(SOURCE_FILE, TARGET_FILE, 'nvupdaterlib', '../../novelyst_updater/src/')
     inliner.run(TARGET_FILE, TARGET_FILE, 'novelystlib', '../../novelyst/src/')
-    inliner.run(TARGET_FILE, TARGET_FILE, 'novxlib', '../../novxlib/src/')
+    inliner.run(TARGET_FILE, TARGET_FILE, 'novxlib', '../../novxlib-Alpha/src/')
     print('Done.')
 
 
