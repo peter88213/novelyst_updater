8edb51ad6754900dd97b859c4171f0fc72f3b87e
peter88213 <peter.88213@t-online.de>
2023-12-16 21:19:51 +0100

    Set the copyright year to 2024

--- tools/build_nv_updater.py
+++ tools/build_nv_updater.py
@@ -6,7 +6,7 @@ this script "inlines" all modules imported from the novxlib package.
 The novxlib project (see https://github.com/peter88213/novxlib)
 must be located on the same directory level as the nv_updater project. 
 
-Copyright (c) 2023 Peter Triesberger
+Copyright (c) 2024 Peter Triesberger
 For further information see https://github.com/peter88213/nv_updater
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
